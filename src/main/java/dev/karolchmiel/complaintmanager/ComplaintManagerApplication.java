3ca8194c1d6ac87cad980cb1f7f4b6a79d424212
Karol Chmiel <karolchmiel24@gmail.com>
2025-05-10 11:13:58 +0200

    Initial commit

---
+++ src/main/java/dev/karolchmiel/complaintmanager/ComplaintManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.karolchmiel.complaintmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplaintManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/karolchmiel/complaintmanager/ComplaintManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.karolchmiel.complaintmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
