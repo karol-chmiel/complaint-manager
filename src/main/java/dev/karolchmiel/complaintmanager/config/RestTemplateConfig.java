b1010a299da045b5de17cd2314ca524cf56c1ed3
Karol Chmiel <karolchmiel24@gmail.com>
2025-05-12 09:31:24 +0200

    Set configurable timeout for RestTemplate.

--- src/main/java/dev/karolchmiel/complaintmanager/config/RestTemplateConfig.java
+++ src/main/java/dev/karolchmiel/complaintmanager/config/RestTemplateConfig.java
@@ -1,15 +1,30 @@
 package dev.karolchmiel.complaintmanager.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
 
+    @Value("${rest.template.timeout:5000}")
+    private int timeout;
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        final var restTemplate = builder.build();
+        restTemplate.setRequestFactory(createTimeoutRequestFactory());
+        return restTemplate;
+    }
+
+    private ClientHttpRequestFactory createTimeoutRequestFactory() {
+        final var factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(timeout);
+        factory.setReadTimeout(timeout);
+        return factory;
     }
 }
